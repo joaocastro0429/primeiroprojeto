e40185ab3f4839fb7aef8aec60c1707f85053ef4
joaocastro <joaocastro0429@gmail.com>
2024-06-29 18:26:15 -0300

    curso de java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme : Top Gun : maverick");
+
+        int anoDeLacamento=2022;
+        System.out.println("Ano de laçamento "+anoDeLacamento);
+        boolean incluidoNoPlano=true;
+        System.out.println(incluidoNoPlano);
+        double notaDoFilme=8.1;
+
+    }
+}
